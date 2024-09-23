3c37814dbaaccdb365c2b3bc87a77f1b7deb694e
saiharshith22 <saiharshithjyothula@gmail.com>
2024-09-23 21:24:17 +0530

    header updated

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,19 +1,49 @@
-import React from "react";
-import "./header.css";
-import Button from "@mui/material/Button";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Stack } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
+import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Tooltip from "@mui/material/Tooltip";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
-import { Stack } from "@mui/material";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import React from "react";
+
+const Logo = ({ xs, md }) => {
+  return (
+    <Stack
+      direction="row"
+      className="logo"
+      sx={{
+        display: {
+          xs: xs,
+          md: md,
+          flexGrow: 1,
+          letterSpacing: "0.083px",
+        },
+      }}
+    >
+      <Typography
+        sx={{
+          color: "#4a4b4c",
+          font: 'normal 700 33px / normal "Montserrat"',
+        }}
+      >
+        EduTech
+      </Typography>
+      <Typography
+        sx={{
+          color: "#19467e",
+          font: 'normal 700 33px / normal "Montserrat"',
+        }}
+      >
+        AI
+      </Typography>
+    </Stack>
+  );
+};
 
 const Header = () => {
   const btnStyle = {
@@ -33,46 +63,23 @@ const Header = () => {
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
+
+  const headerItems = [
+    "Home",
+    "Product",
+    "Pricing",
+    "AI Book Store",
+    "Resources",
+  ];
   return (
-    // <nav className="header">
-    //   <Stack direction="row" className="logo">
-    //     <span className="logo_edutech">EduTech</span>
-    //     <span className="logo_ai">AI</span>
-    //   </Stack>
-    //   <Stack className="header_items" direction="row">
-    //     <span className="item_text">Home</span>
-    //     <span className="item_text">Product</span>
-    //     <span className="item_text">Pricing</span>
-    //     <span className="item_text">AI Books Store</span>
-    //     <span className="item_text">Resources</span>
-    //   </Stack>
-    //   <Stack direction="row" sx={{ gap: "1rem" }}>
-    //     <Button sx={{ ...btnStyle, background: "#FCFCFC" }} variant="outlined">
-    //       Log in
-    //     </Button>
-    //     <Button sx={{ ...btnStyle, background: "#054BB4" }} variant="contained">
-    //       Sign up
-    //     </Button>
-    //   </Stack>
-    // </nav>
-    <AppBar position="static">
+    <AppBar
+      position="static"
+      sx={{ bgcolor: "white", margin: "30px 0" }}
+      elevation={0}
+    >
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <Stack
-            direction="row"
-            className="logo"
-            sx={{ display: { xs: "none", md: "flex" } }}
-          >
-            <span className="logo_edutech">EduTech</span>
-            <span className="logo_ai">AI</span>
-          </Stack>
-          {/* 
-          xs, extra-small: 0px 
-          sm, small: 600px 
-          md, medium: 900px 
-          lg, large: 1200px 
-          xl, extra-large: 1536px 
-          */}
+          <Logo xs="none" md="flex" />
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
@@ -80,7 +87,7 @@ const Header = () => {
               aria-controls="menu-appbar"
               aria-haspopup="true"
               onClick={handleOpenNavMenu}
-              color="inherit"
+              color="#595959"
             >
               <MenuIcon />
             </IconButton>
@@ -100,66 +107,55 @@ const Header = () => {
               onClose={handleCloseNavMenu}
               sx={{ display: { xs: "block", md: "none" } }}
             >
-              <MenuItem onClick={handleCloseNavMenu}>
-                <Typography sx={{ textAlign: "center" }}>Home</Typography>
-              </MenuItem>
-
-              <MenuItem onClick={handleCloseNavMenu}>
-                <Typography sx={{ textAlign: "center" }}>Product</Typography>
-              </MenuItem>
-              <MenuItem onClick={handleCloseNavMenu}>
-                <Typography sx={{ textAlign: "center" }}>Pricing</Typography>
-              </MenuItem>
-              <MenuItem onClick={handleCloseNavMenu}>
-                <Typography sx={{ textAlign: "center" }}>
-                  AI Books Store
-                </Typography>
-              </MenuItem>
-              <MenuItem onClick={handleCloseNavMenu}>
-                <Typography sx={{ textAlign: "center" }}>Resources</Typography>
-              </MenuItem>
+              {headerItems.map((item) => (
+                <MenuItem key={item} onClick={handleCloseNavMenu}>
+                  <Typography
+                    className="item_text"
+                    sx={{ textAlign: "center" }}
+                  >
+                    {item}
+                  </Typography>
+                </MenuItem>
+              ))}
             </Menu>
           </Box>
-          <Stack
-            direction="row"
-            className="logo"
-            sx={{ display: { xs: "flex", md: "none" } }}
+          <Logo xs="flex" md="none" />
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", md: "flex", gap: "2rem" },
+            }}
           >
-            <span className="logo_edutech">EduTech</span>
-            <span className="logo_ai">AI</span>
-          </Stack>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            <Button
-              onClick={handleCloseNavMenu}
-              sx={{ my: 2, color: "white", display: "block" }}
-            >
-              Home
-            </Button>
-            <Button
-              onClick={handleCloseNavMenu}
-              sx={{ my: 2, color: "white", display: "block" }}
-            >
-              Product
-            </Button>
-            <Button
-              onClick={handleCloseNavMenu}
-              sx={{ my: 2, color: "white", display: "block" }}
-            >
-              Pricing
-            </Button>
-            <Button
-              onClick={handleCloseNavMenu}
-              sx={{ my: 2, color: "white", display: "block" }}
-            >
-              AI Books Store
-            </Button>
-            <Button
-              onClick={handleCloseNavMenu}
-              sx={{ my: 2, color: "white", display: "block" }}
-            >
-              Resources
-            </Button>
+            {headerItems.map((item) => (
+              <Button
+                key={item}
+                onClick={handleCloseNavMenu}
+                sx={{
+                  my: 2,
+                  display: "block",
+                  color: "#595959 ",
+                  font: 'normal 400 23px / normal "Inter"',
+                  textTransform: "none",
+                }}
+              >
+                {item}
+              </Button>
+            ))}
           </Box>
+          <Stack direction="row" sx={{ gap: "1rem" }}>
+            <Button
+              sx={{ ...btnStyle, background: "#FCFCFC" }}
+              variant="outlined"
+            >
+              Log in
+            </Button>
+            <Button
+              sx={{ ...btnStyle, background: "#054BB4" }}
+              variant="contained"
+            >
+              Sign up
+            </Button>
+          </Stack>
         </Toolbar>
       </Container>
     </AppBar>
