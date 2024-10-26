29c809486fb05196f14ac2c51dbf6c46cc528d33
saiharshith22 <saiharshithjyothula@gmail.com>
2024-10-27 02:12:06 +0530

    removed unused code

--- src/components/header/SidebarComponent.js
+++ src/components/header/SidebarComponent.js
@@ -1,28 +1,16 @@
-import * as React from "react";
-import PropTypes from "prop-types";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Button } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
 import Drawer from "@mui/material/Drawer";
 import IconButton from "@mui/material/IconButton";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import MailIcon from "@mui/icons-material/Mail";
-import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import McqGenerator from "../../pages/McqGenerator";
-import SidebarContent from "./SidebarContent";
-import { Button } from "@mui/material";
+import * as React from "react";
 import BellIcon from "../../assets/Images/bell icon.svg";
 import UserIcon from "../../assets/Images/User.svg";
-import PersonIcon from "../../assets/Images/PersonPic.png";
-import { styled } from "@mui/material/styles";
+import McqGenerator from "../../pages/McqGenerator";
+import SidebarContent from "./SidebarContent";
 
 const drawerWidth = 300;
 
@@ -46,39 +34,6 @@ function ResponsiveDrawer(props) {
     }
   };
 
-  const drawer = (
-    <div>
-      <Toolbar />
-      <Divider />
-      <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
-
-  // Remove this const when copying and pasting into your project.
   const container =
     window !== undefined ? () => window().document.body : undefined;
   const btnStyle = {
